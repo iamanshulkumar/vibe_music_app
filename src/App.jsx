8cc7c8fa49215bafecdec48f21fb0ba059637944
iamanshulkumar <anshulkm17@gmail.com>
2022-12-05 16:51:01 +0530

    Done with basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,9 @@ const App = () => {
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
           <div className="flex-1 h-fit pb-40">
-            <Routes>
-              <Route path="/" element={<Discover />} />
+          {/* General route structure */}
+            <Routes> 
+              <Route path="/" element={<Discover />} /> {/* Home page */}
               <Route path="/top-artists" element={<TopArtists />} />
               <Route path="/top-charts" element={<TopCharts />} />
               <Route path="/around-you" element={<AroundYou />} />
