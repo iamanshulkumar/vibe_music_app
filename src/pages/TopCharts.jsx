5bf7047fdd6932227ad1353bed51ccdab35e41ed
iamanshulkumar <anshulkm17@gmail.com>
2022-12-14 20:12:11 +0530

    Doing final tweaks

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -13,21 +13,21 @@ export const shazamCoreApi = createApi({
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/v1/charts/world' }),
     getSongsByGenre: builder.query({ query: (genre) => `/v1/charts/genre-world?genre_code=${genre}` }),
-    getSongDetails: builder.query({ query: ({ songid }) => `/v1/tracks/details?track_id=${songid}` }),
-    getSongRelated: builder.query({ query: ({ songid }) => `/v1/tracks/related?track_id=${songid}` }),
-    // having issue in fetching getArtistDetails as v1 is deprecated
-    getArtistDetails: builder.query({ query: (artistId) => `/v2/artists/details?artist_id=${artistId}` }),
     getSongsByCountry: builder.query({ query: (countryCode) => `/v1/charts/country?country_code=${countryCode}` }),
     getSongBySearch: builder.query({ query: (searchTerm) => `/v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/v1/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/v1/tracks/related?track_id=${songid}` }),
+    // having issue in fetching getArtistDetails as v1 is deprecated + having CORS issue
+    getArtistDetails: builder.query({ query: (artistId) => `/v2/artists/details?artist_id=${artistId}` }),
   }),
 });
 
 export const {
   useGetTopChartsQuery,
   useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
   useGetSongDetailsQuery,
+  useGetSongBySearchQuery,
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery, // still not working
-  useGetSongsByCountryQuery,
-  useGetSongBySearchQuery,
 } = shazamCoreApi;
